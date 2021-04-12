f1a304306ced74cdf9d89f941ec59759e1f0e71c
Zuhri Utama <zuhriutama@gmail.com>
2021-04-13 05:40:10 +0700

    update facebook link and video

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -85,14 +85,14 @@
         <!--Footer-->
         <footer class="text-gray-600 body-font bg-white">
             <div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-                <a href="https://web.facebook.com/groups/673819220093340" target="_blank" class="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+                <a href="https://qacjakarta.id" target="_blank" class="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
                 <img class="w-10 inline" src="{{ asset('qacjakarta.png') }}" />
                 <span class="ml-3 text-xl">{{ config('app.name') }}</span>
                 </a>
                 <p class="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© 2020 {{ config('app.name') }}
                 </p>
                 <span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-                <a href="https://web.facebook.com/groups/673819220093340" target="_blank" class="text-gray-500">
+                <a href="https://www.facebook.com/QACJakarta" target="_blank" class="text-gray-500">
                     <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
                     <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
                     </svg>
