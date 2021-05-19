27274035ec2a816c3b437a83a79cc531a80cd208
Zuhri Utama <zuhriutama@gmail.com>
2021-05-19 09:49:10 +0700

    bug fix on dashboard member

--- resources/views/member/dashboard.blade.php
+++ resources/views/member/dashboard.blade.php
@@ -45,7 +45,7 @@
                         <td class="px-2 py-4 text-sm text-gray-500">
 
                             <div class="flex justify-start space-x-1">
-                                <a href="{{ route('member.batches.detail', $batch->id) }}" class="border-2 border-indigo-200 rounded-md p-1">
+                                <a href="{{ route('member.batches.detail', $batch->pivot->id) }}" class="border-2 border-indigo-200 rounded-md p-1">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-4 w-4 text-indigo-500">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
