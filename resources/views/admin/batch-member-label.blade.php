e9f11e1f37d7cad1c373535d3798ba0f4045ee4a
uthadehikaru <zuhriutama@gmail.com>
2021-06-12 10:37:19 +0700

    update label print layout

--- resources/views/admin/batch-member-label.blade.php
+++ resources/views/admin/batch-member-label.blade.php
@@ -1,16 +1,54 @@
-<x-guest-layout>
-<div class="grid grid-cols-2">
-    @foreach($batch->members as $member)
-    <div class="p-2 text-center border border-black">
-    <p class="font-bold">{{ $member->full_name }}</p>
-    <p class="my-2">{{ $member->address }}</p>
-    <p>{{ $member->phone }}</p>
-    </div>
-    @endforeach
-</div>
-<x-slot name="scripts">
-<script>
-window.print();
-</script>
-</x-slot>
-</x-guest-layout>
+<html>
+    <head>
+    <style>
+* {
+  box-sizing: border-box;
+}
+
+/* Create two equal columns that floats next to each other */
+.column {
+  float: left;
+  width: 50%;
+  padding: 10px;
+  text-align:center;
+  border-color: black;
+    border-width: 1px;
+    border-style: solid;
+}
+
+/* Clear floats after the columns */
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+@media print{
+    tr{
+        page-break-inside:avoid;
+    }
+}
+</style>
+    </head>
+    <body>
+        <table width="100%" border="1" style="border-collapse: collapse;text-align:center;">
+        <tr>
+        @php
+        $i=0;
+        @endphp
+            @foreach($batch->members as $member)
+            @if(($i++)%2==0)
+                </tr><tr>
+            @endif
+            <td width="50%">
+            <h4>{{ $member->full_name }}</h4>
+            <p>{{ $member->address }}</p>
+            <p>{{ $member->phone }}</p>
+            </td>
+            @endforeach
+        </tr>
+        </table>
+        <script>
+        window.print();
+        </script>
+    </body>
+</html>
