7d465362d3a5ce3a2ad1ccb1f9f79bd69aee8faf
uthadehikaru <zuhriutama@gmail.com>
2021-08-12 09:21:38 +0700

    only admin can see footer link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,9 +41,11 @@
                     <div class="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
                     <p class="text-gray-500 text-sm text-center sm:text-left">© 2021 QAC Jakarta
                     </p>
+                    @if(Auth::user()->isAdmin)
                     <span class="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
                         <a class="text-gray-500 pointer text-sm" href="{{ route('admin.jobs.index') }}">Email Processor</a>
                     </span>
+                    @endif
                     </div>
                 </div>
             </footer>
