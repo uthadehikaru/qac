e9b10c05d53dd178593bb972bede9a14f2a24010
uthadehikaru <zuhriutama@gmail.com>
2021-10-07 00:53:24 +0700

    update qac banner image

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,7 +16,7 @@
             </div>
             <!--Right Col-->
             <div class="w-full md:w-3/5 py-6 text-center">
-            <img class="w-full md:w-3/5 z-50" src="hero.png" />
+            <img class="w-full md:w-3/5 z-50" src="qac banner.png" />
             </div>
         </div>
         </div>
