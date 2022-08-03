652b9664b72f99dc30de471d51f6bee79659e472
zuhriutama <zuhriutama@gmail.com>
2022-08-03 07:44:20 +0700

    bug fix when share event for course

--- app/Http/Controllers/Admin/EventController.php
+++ app/Http/Controllers/Admin/EventController.php
@@ -148,9 +148,11 @@ class EventController extends Controller
         $count = 0;
         if($event->course){
             $memberBatches = MemberBatch::with('member.user')
+            ->join('members','member_batch.member_id','members.id')
+            ->join('users','members.user_id','users.id')
             ->whereRaw("member_batch.status='6' AND EXISTS(SELECT 1 from batches b WHERE b.id=member_batch.batch_id AND b.course_id=".$event->course_id.")" )
-            ->orderBy('login_at','desc')
-            ->orderBy('updated_at','desc')
+            ->orderBy('users.login_at','desc')
+            ->orderBy('users.updated_at','desc')
             ->get();
             foreach($memberBatches as $memberBatch){
                 $memberBatch->member->user->notify(new EventCreated($event));
