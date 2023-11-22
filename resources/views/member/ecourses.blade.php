5392f6f1bd41c720c13642250e042a98a5237746
zuhriutama <zuhriutama@gmail.com>
2023-11-22 10:29:00 +0700

    set 2 column for ecourse and set image

--- resources/views/member/ecourses.blade.php
+++ resources/views/member/ecourses.blade.php
@@ -8,12 +8,15 @@
     <x-panel>
         <div class="flex flex-wrap -m-4">
             @foreach ($ecourses as $ecourse)
-            <div class="xl:w-1/3 md:w-1/2 p-4">
+            <div class="md:w-1/2 p-4">
                 <div class="bg-gray-100 p-6 rounded-lg">
-                    <img class="h-40 rounded w-full object-cover object-center mb-6" src="{{ $ecourse->imageUrl('thumbnail') }}" alt="content" />
-                    <h2 class="text-lg text-gray-900 font-medium title-font mb-4">{{ $ecourse->title }}</h2>
+                    <img class="
+                     rounded w-full object-cover object-center mb-6" src="{{ $ecourse->imageUrl('thumbnail') }}" alt="content" />
+                    <h2 class="text-lg text-gray-900 font-bold title-font mb-4">{{ $ecourse->title }}</h2>
                     <p class="leading-relaxed text-base mb-4">{{ $ecourse->description }}</p>
-                    <a href="{{ route('member.ecourses.show', $ecourse->slug) }}" class="bg-green-500 rounded rounded-full p-2 text-white text-sm">Start Course</a>
+                    <div class="flex justify-center">
+                    <a href="{{ route('member.ecourses.show', $ecourse->slug) }}" class="bg-green-500 rounded rounded-full p-2 text-white hover:opacity-75">Start Course</a>
+                    </div>
                 </div>
             </div>
             @endforeach
