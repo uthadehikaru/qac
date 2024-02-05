a1a9ce6f46e055b7f0754af94b0bf1c43f50b783
zuhriutama <zuhriutama@gmail.com>
2024-02-05 09:19:15 +0700

    add video to welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -44,14 +44,19 @@
                 <div class="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
             </div>
             <div class="flex flex-wrap">
-                <div class="w-full sm:w-1/2 p-6">
+                <div class="w-full sm:w-1/3 p-6">
                     <video width="400" height="800" controls controlsList="nodownload">
-                        <source src="{{ asset('media/why1.mp4') }}" type="video/mp4">
+                        <source src="{{ asset('media/why1.mp4') }}?v=20240205" type="video/mp4">
                     </video>
                 </div>
-                <div class="w-full sm:w-1/2 p-6">
+                <div class="w-full sm:w-1/3 p-6">
                     <video width="400" height="800" controls controlsList="nodownload">
-                        <source src="{{ asset('media/why2.mp4') }}" type="video/mp4">
+                        <source src="{{ asset('media/why2.mp4') }}?v=20240205" type="video/mp4">
+                    </video>
+                </div>
+                <div class="w-full sm:w-1/3 p-6">
+                    <video width="400" height="800" controls controlsList="nodownload">
+                        <source src="{{ asset('media/why3.mp4') }}?v=20240205" type="video/mp4">
                     </video>
                 </div>
             </div>
