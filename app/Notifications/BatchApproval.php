c64ac93fc587f2c9ea004591f91c31b0ab3711c3
zuhriutama <zuhriutama@gmail.com>
2024-05-17 14:33:13 +0700

    Revert "Revert "queue mail""
    This reverts commit d555117c04d1e7266fcb6951daa8c0ad9300294e.

--- app/Notifications/BatchApproval.php
+++ app/Notifications/BatchApproval.php
@@ -4,10 +4,11 @@ namespace App\Notifications;
 
 use App\Models\MemberBatch;
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class BatchApproval extends Notification
+class BatchApproval extends Notification implements ShouldQueue
 {
     use Queueable;
 
