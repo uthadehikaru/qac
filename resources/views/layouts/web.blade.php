0876740c667590c90db5960198bbfdc417e97941
zuhri utama <zuhriutama@gmail.com>
2025-06-17 12:52:38 +0700

    update header

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -38,10 +38,21 @@
         <div class="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
             <div class="pl-4 flex items-center">
             <a class="no-underline hover:no-underline font-bold text-2xl lg:text-4xl" href="{{ url('') }}">
-                <img class="h-14 inline" src="{{ asset('qacnew.png') }}" />
+                <img class="h-8 md:h-14 inline" src="{{ asset('qacnew.png') }}" />
             </a>
             </div>
-            <div class="block lg:hidden pr-4">
+            <ul class="list-reset flex justify-center flex-1 items-center">
+                <li class="mr-3">
+                <a class="inline-block py-2 px-2 md:py-2 md:px-4 text-black no-underline text-center" href="{{ url('/') }}">Kelas</a>
+                </li>
+                <li class="mr-3">
+                <a class="inline-block py-2 px-2 md:py-2 md:px-4 text-black no-underline text-center" href="{{ url('/') }}">Program Alumni</a>
+                </li>
+                <li class="mr-3">
+                <a class="inline-block py-2 px-2 md:py-2 md:px-4 text-black no-underline text-center" href="{{ url('/') }}">Event</a>
+                </li>
+            </ul>
+            <div class="block pr-4">
             <button id="nav-toggle" class="flex items-center p-1 text-pink-800 hover:text-gray-900 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
                 <svg class="fill-current h-6 w-6" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                 <title>Menu</title>
@@ -49,57 +60,19 @@
                 </svg>
             </button>
             </div>
-            <div class="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden mt-2 lg:mt-0 bg-white lg:bg-transparent text-black p-4 lg:p-0 z-20" id="nav-content">
-            <ul class="list-reset lg:flex justify-end flex-1 items-center">
-                <li class="mr-3">
-                <a class="inline-block py-2 px-4 text-black font-bold no-underline" href="{{ url('/') }}">Beranda</a>
-                </li>
-                <li class="mr-3">
-                <a class="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4" href="{{ url('/') }}#about">Tentang Kami</a>
-                </li>
+            <div class="w-full flex-grow hidden mt-2 text-black p-4 z-20" id="nav-content">
+            <ul class="list-reset justify-end flex-1 items-center">
                 <li class="mr-3">
                 <a class="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4" href="{{ url('/') }}#testimonial">Testimoni</a>
-                </li>
-                <li class="mr-3">
-                <a class="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4" href="{{ url('/') }}#courses">Kelas</a>
                 </li>
                 <li class="mr-3">
                 <a class="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4" href="{{ route('faq') }}">FAQ</a>
                 </li>
                 <li class="mr-3">
-                <a class="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4" href="{{ route('event.list') }}">Event</a>
-                </li>
-                <li class="mr-3">
-                    @guest
-                    <a id="navAction" class="inline-block text-white no-underline hover:opacity-75 hover:text-underline py-2 px-4 bg-red-800 rounded-full w-40 text-center" href="javascript:;" onclick="toggleModal()">Masuk</a>
+                    @guest    
+                    <a class="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4" href="{{ route('login') }}">Masuk</a>
                     @else
-                    <div class="dropdown inline-block relative">
-                    <a id="navAction" class="inline-block text-white no-underline hover:opacity-75 hover:text-underline py-2 px-4 bg-red-800 rounded-full w-40 text-center" href="#">@lang('Hi'), {{ Auth::user()->name }}</a>
-                        <ul class="dropdown-menu absolute hidden text-gray-700 pt-1 w-full">
-                            <li class="">
-                                @can('is-member')
-                                <a class="rounded-t bg-red-800 text-white hover:bg-primary py-2 px-4 block whitespace-no-wrap"
-                                href="{{ route('member.ecourses.index') }}">
-                                My Courses
-                                </a>
-                                <a class="rounded-t bg-red-800 text-white hover:bg-primary py-2 px-4 block whitespace-no-wrap"
-                                href="{{ route('member.dashboard') }}">
-                                Kelas QAC
-                                </a>
-                                <a class="rounded-t bg-red-800 text-white hover:bg-primary py-2 px-4 block whitespace-no-wrap"
-                                href="{{ route('member.profile') }}">
-                                Profil
-                                </a>
-                                @endcan
-                                @can('is-admin')
-                                <a class="rounded-t bg-red-800 text-white hover:bg-primary py-2 px-4 block whitespace-no-wrap"
-                                href="{{ route('admin.dashboard') }}">
-                                Dashboard
-                                </a>
-                                @endcan
-                            </li>
-                        </ul>
-                    </div>
+                    <a class="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4" href="{{ route('logout') }}">Keluar</a>
                     @endguest
                 </li>
             </ul>
