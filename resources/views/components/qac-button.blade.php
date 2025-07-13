322c185ea02a0b579421003def0a0209e08ae638
zuhri utama <zuhriutama@gmail.com>
2025-07-13 13:40:23 +0700

    fix ecertificate button

--- resources/views/components/qac-button.blade.php
+++ resources/views/components/qac-button.blade.php
@@ -1,3 +1,3 @@
-<a {{ $attributes->merge(['class' => 'bg-[#7b0c00] text-white font-bold rounded-full py-2 px-4 text-sm']) }}>
+<a {{ $attributes->merge(['class' => 'bg-[#7b0c00] text-white font-bold rounded-full py-2 px-4 text-xs md:text-sm']) }}>
     {{ $slot }}
 </a>
