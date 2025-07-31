b752878fc23a9bdaf6903abb45c5d82768aa36b4
zuhri utama <zuhriutama@gmail.com>
2025-07-31 14:02:29 +0700

    update donais

--- resources/views/donasi.blade.php
+++ resources/views/donasi.blade.php
@@ -2,8 +2,8 @@
 <section id="faq" class="bg-white pt-24 text-gray-700">
     <div class="container px-5 mx-auto">
         <img src="{{ asset('images/donasi banner.png') }}" alt="Donasi Banner" class="w-full md:w-2/3 mx-auto">
-        <p class="text-black text-base md:text-xl">Donasi ini akan dipakai untuk <span class="font-bold">mendukung kegiatan-kegiatan QAC</span>. 
-        Kegiatan kami seputar <span class="font-bold">pemberdayaan muslim</span> dan <span class="font-bold">membaca Al-Qur'an sampai tadabbur bahasa Arabnya</span>. Kami berharap dengan setiap hari terpapar tadabbur Al-Qur'an akan berpengaruh kepada hati dan perilaku sehari-hari.</p>
+        <p class="text-black text-base md:text-xl text-justify">Donasi ini akan dipakai untuk <span class="font-bold">mendukung kegiatan-kegiatan QAC</span>. 
+        Kegiatan kami seputar <span class="font-bold text-justify">pemberdayaan muslim</span> dan <span class="font-bold">membaca Al-Qur'an sampai tadabbur bahasa Arabnya</span>. Kami berharap dengan setiap hari terpapar tadabbur Al-Qur'an akan berpengaruh kepada hati dan perilaku sehari-hari.</p>
         <p class="pt-12 text-center w-full text-base md:text-xl">Donasi via :</p>
         <a href="https://wa.me/{{ \App\Models\System::value('whatsapp') }}" target="_blank" class="flex justify-center"><img src="{{ asset('images/waadmin.png') }}" alt="WA Admin QAC" class="w-1/2 md:w-1/3"></a>
         <img src="{{ asset('images/donasi quote.png') }}" alt="Donasi Quote" class="w-full md:w-1/2 mx-auto">
