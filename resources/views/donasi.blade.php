8924b1d22be1d802560d6021c9986798f441ba9c
zuhri utama <zuhriutama@gmail.com>
2025-08-08 15:16:14 +0700

    udpate wa on donasi

--- resources/views/donasi.blade.php
+++ resources/views/donasi.blade.php
@@ -5,7 +5,7 @@
         <p class="text-black text-base md:text-xl text-justify w-full md:w-2/3 mx-auto">Donasi ini akan dipakai untuk <span class="font-bold">mendukung kegiatan-kegiatan QAC</span>. 
         Kegiatan kami seputar <span class="font-bold text-justify">pemberdayaan muslim</span> dan <span class="font-bold">membaca Al-Qur'an sampai tadabbur bahasa Arabnya</span>. Kami berharap dengan setiap hari terpapar tadabbur Al-Qur'an akan berpengaruh kepada hati dan perilaku sehari-hari.</p>
         <p class="pt-12 text-center w-full text-base md:text-xl">Donasi via :</p>
-        <a href="https://wa.me/{{ \App\Models\System::value('whatsapp') }}" target="_blank" class="flex justify-center"><img src="{{ asset('images/waadmin.png') }}" alt="WA Admin QAC" class="w-1/2 md:w-1/3"></a>
+        <a href="https://wa.me/{{ \App\Models\System::value('whatsapp_ecourse') }}" target="_blank" class="flex justify-center"><img src="{{ asset('images/waadmin.png') }}" alt="WA Admin QAC" class="w-1/2 md:w-1/3"></a>
         <img src="{{ asset('images/donasi quote.png') }}" alt="Donasi Quote" class="w-full md:w-1/2 mx-auto">
     </div>
 </section>
